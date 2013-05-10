46facee808ecbb0dc6cafd9b4889a86e4df55c64
Igor Novak <vorian@yandex-team.ru>
2013-05-10 14:51:09 +0000

    up bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 git pull
-for f in `find . -type f -name '\.*' -exec basename {} \;`
+for f in `find . -name '\.*' -exec basename {} \; | grep -v '\(git\|\.\)$'`
 do
     ln -fs $PWD/$f ~/$f
 done
