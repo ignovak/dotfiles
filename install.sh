c27b66d4962e340319613b041626a0f312bd6b6a
Igor Novak <bezengi@gmail.com>
2015-10-08 09:33:19 +0200

    Backup original dotfiles in install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,24 @@ cd "$(dirname "$0")"
 git pull
 for f in `find . -name '\.*' -exec basename {} \; | grep -v 'git\|\.$\|bash\|exports\|prompt'`
 do
+    if [ -f ~/$f ]; then
+        mv ~/$f ~/$f.bk
+        echo Save ~/$f.bk
+    fi
     ln -fs $PWD/$f ~
 done
 
-GIT_AUTHOR_NAME=`git config user.name`
-GIT_AUTHOR_EMAIL=`git config user.email`
-[ -f ~/.gitconfig ] || ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 mkdir -p ~/.config/git/
-git config -f ~/.config/git/config user.name "$GIT_AUTHOR_NAME"
-git config -f ~/.config/git/config user.email "$GIT_AUTHOR_EMAIL"
+touch ~/.config/git/config
+git config -f ~/.config/git/config user.name `git config user.name`
+git config -f ~/.config/git/config user.email `git config user.email`
+if [ -f ~/.gitconfig ]; then
+    mv ~/.gitconfig ~/.gitconfig.bk
+    echo Warning: ~/.gitconfig is backed up with ~/.gitconfig.bk and replaced with a symlink to ~/$PWD/.gitconfig
+    echo Use diff ~/.gitconfig.bk ~/.gitconfig in order to see check changes
+    echo Use ~/.config/git/config for personal git settings, i.e. name or email
+fi
+ln -s ~/$PWD/.gitconfig ~/.gitconfig
 
 source ~/.profile
 
